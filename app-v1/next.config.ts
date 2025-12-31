da56fcecc31f53546ad4511cdfa59e73b8a7912f
Chrisbeep98 <christiansandovaldesign@gmail.com>
2025-12-31 11:29:08 -0500

    Actualizar configuración para nuevo repo público

--- app-v1/next.config.ts
+++ app-v1/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // Desactiva la optimización de imágenes que requiere servidor
   },
-  basePath: "/NEVADOTREKV3.0", // Nombre de tu repositorio para cargar assets correctamente
-  assetPrefix: "/NEVADOTREKV3.0/", // Prefijo para los assets
+  basePath: "/NEVADOTREK.COM-V3.0-PROYECY", // Nombre de tu repositorio para cargar assets correctamente
+  assetPrefix: "/NEVADOTREK.COM-V3.0-PROYECY/", // Prefijo para los assets
 };
 
 export default nextConfig;
